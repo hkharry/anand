e401676ed866ee460fe9ce6b81cdc7b54dad39fb
hari <harry.hvp@gmail.com>
2017-08-07 21:02:31 +0530

    first commit

---
+++ app/src/main/java/com/example/hari/myapplication/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.hari.myapplication;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Rect;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    int location[] = new int[2];
+
+    MapView mapView;
+    GoogleMap map;
+    Activity activityCompat;
+    EditText latv,lonv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        activityCompat=this;
+
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) findViewById(R.id.map_view);
+        latv=(EditText)findViewById(R.id.lat);
+        lonv=(EditText)findViewById(R.id.lon);
+
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+
+                Location latLng=map.getMyLocation();
+                double lat=latLng.getLatitude();
+                double lon=latLng.getLongitude();
+
+                latv.setText(String.valueOf(lat));
+                lonv.setText(String.valueOf(lon));
+                // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+                MapsInitializer.initialize(activityCompat);
+
+                // Updates the location and zoom of the MapView
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+                map.animateCamera(cameraUpdate);
+
+            }
+        });
+
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        map.setMyLocationEnabled(true);
+
+
+
+
+
+}
+
+
+    private int getRelativeLeft(View myView) {
+        if (myView.getParent() == myView.getRootView())
+            return myView.getLeft();
+        else
+            return myView.getLeft() + getRelativeLeft((View) myView.getParent());
+    }
+
+    private int getRelativeTop(View myView) {
+        if (myView.getParent() == myView.getRootView())
+            return myView.getTop();
+        else
+            return myView.getTop() + getRelativeTop((View) myView.getParent());
+    }
+
+}
+
+
+
